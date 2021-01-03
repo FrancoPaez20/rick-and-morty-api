294ff49423b6d3be67eb0ee530a23e9d1b3501b3
Franco <franco.p.0109@gmail.com>
2021-01-02 22:30:28 -0300

    terminado el pop up

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let grid = document.getElementById('grid');
-let popUp = document.getElementById('pop-up');
-
-async function getAllCharacters() {
-
+const popUp = document.getElementById('pop-up');
+async function getCharactersCount() {
 	const URL_BASE = 'https://rickandmortyapi.com/api/character/';
 	const DATA = await (await fetch(`${URL_BASE}`)).json();
 	const TOTAL = await DATA.info.count;
@@ -10,11 +7,10 @@ async function getAllCharacters() {
 }
 
 async function getCharacter(id) {
-
-	let real_id = id + 1;
-	const URL_CHARACTER = `https://rickandmortyapi.com/api/character/${real_id}`;
+	const URL_CHARACTER = `https://rickandmortyapi.com/api/character/${id + 1}`;
 	const DATA = await (await fetch(URL_CHARACTER)).json();
 	return {
+		id: DATA.id,
 		name: DATA.name,
 		image: DATA.image,
 		status: DATA.status,
@@ -22,15 +18,15 @@ async function getCharacter(id) {
 		type: DATA.type,
 		gender: DATA.gender,
 		origin_name: DATA.origin.name,
-		origin_type: DATA.origin.type,
-		origin_dimenison: DATA.origin.dimension,
+		// origin_type: DATA.origin.type,
+		// origin_dimenison: DATA.origin.dimension,
 		episode: DATA.episode
 	};
 }
 
 async function printCharacters() {
-
-	const totalCharacters = await getAllCharacters();
+	const grid = document.getElementById('grid');
+	const totalCharacters = await getCharactersCount();
 	const statusHtml = {
 		'Alive': '<div class="circle alive"></div>',
 		'Dead': '<div class="circle dead"></div>',
@@ -39,38 +35,51 @@ async function printCharacters() {
 
 	// for (let i = 0; i < totalCharacters; i++) {
 	for (let i = 0; i < 10; i++) {
-
 		const CHARACTER = await getCharacter(i);
-
 		grid.innerHTML += `
-		<a href="#" class="grid__item">
-		<figure class="grid__item__image">
-		<img src="${CHARACTER.image}" alt="${CHARACTER.name}">
-		</figure>
-		<div class="grid__item__info">
-		${statusHtml[CHARACTER.status]}
-		<h2>${CHARACTER.name}</h2>
-		</div>
-		</a>`;
+		<div class="grid__item" data-id="${i + 1}">
+			<figure class="grid__item__image">
+				<img src="${CHARACTER.image}" alt="${CHARACTER.name}">
+			</figure>
+			<div class="grid__item__info">
+				${statusHtml[CHARACTER.status]}
+				<h2>${CHARACTER.name}</h2>
+			</div>
+		</div>`;
 	}
 
 	let gridItem = document.querySelectorAll('.grid__item').forEach(item => {
+		item.addEventListener('click', async function revealPopUp() {
+			let popUpContainer = document.getElementById('pop-up-container');
+			popUpContainer.style.display = "flex";
+			let id = parseInt(item.getAttribute('data-id'));
+			const popUp = document.getElementById('pop-up');
+			const CHARACTER = await getCharacter(id - 1);
 
-		item.addEventListener('click', function revealPopUp() {
-
-			let popUp = document.getElementById('pop-up-container');
-			popUp.style.display = "block";
+			popUp.innerHTML = `
+			<img src="${CHARACTER.image}" alt="${CHARACTER.name}">
+			
+			<h2>${CHARACTER.name} #${CHARACTER.id}</h2>
+			<p>Status: ${CHARACTER.status}</p>
+			<p>Specie: ${CHARACTER.species}</p>
+			<p>Gender: ${CHARACTER.gender}</p>
+			<p>Origin: ${CHARACTER.origin_name}</p>
+			<p>Type: ${CHARACTER.type}</p>
+			<p>Appearances: ${Object.keys(CHARACTER.episode).length} episodes</p>	
+			`
+			// <p>${CHARACTER.origin_type}</p>
+			// <p>${CHARACTER.origin_dimension}</p>
 		});
 	});
 }
 
 printCharacters();
 
-let close = document.getElementById('close');
+const close = document.getElementById('close');
 
 close.addEventListener('click', function closePopUp() {
 
-	let popUp = document.getElementById('pop-up-container');
-	popUp.style.display = "none";
+	const popUpContainer = document.getElementById('pop-up-container');
+	popUpContainer.style.display = "none";
 })
-// console.log(Object.keys(CHARACTER.episode).length);
+// console.log();
